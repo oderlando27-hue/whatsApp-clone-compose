b222711bad99edcef0ac707a6bb909432d085f47
oderlando27-hue <oderlando27@gmail.com>
2026-07-28 22:47:14 -0300

    Update MainActivity.kt

--- app/src/main/kotlin/io/getstream/whatsappclone/MainActivity.kt
+++ app/src/main/kotlin/io/getstream/whatsappclone/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
+androidx.core.app.NotificationManagerCompat.from(this).cancelAll()
     setContent { WhatsAppCloneMain(appComposeNavigator) }
   }
 }
